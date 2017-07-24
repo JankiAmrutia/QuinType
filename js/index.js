9920d16d4a0fdfb344fbbcdef13ac13443857cf6
Janki Amrutiya <jamrutiya@deloitte.com>
2017-07-24 11:27:31 +0530

    changes

--- js/index.js
+++ js/index.js
@@ -40,7 +40,7 @@ function culture(data){
 
 function international(data){
     var display= document.getElementById("content2");
-     display.innerHTML= '<div class="row"><div class="content col-md-10 col-md-offset-1"> <div class="col-md-3 business-content"><img class="travel" src="http://quintype-01.imgix.net/quintype-demo/2015-11/21006f30-9d7e-4219-ba96-4b2fd945e53c/cuba14.jpg"/><p class="travel-content">' + data.sections[0]['name'] + '</p>' +'<p class="sub-heading">' + data['headline'] + '</p>' + '<p class="sub-content">' + data['headline'] + '</p>' + '<p class="author-name">' + data['author-name'] + '</p>' + '<p class="author-name">' + 'Posted 15 mins ago' + '</p>' +  '<div class="col-md-4 travel-content"> </div><div class="col-md-3 publish"> </div> </div> </div>'
+     display.innerHTML= '<div class="row"><div class="col-md-10 col-md-offset-1"> <div class="col-md-3 business-content"><img class="travel" src="http://quintype-01.imgix.net/quintype-demo/2015-11/21006f30-9d7e-4219-ba96-4b2fd945e53c/cuba14.jpg"/><p class="travel-content">' + data.sections[0]['name'] + '</p>' +'<p class="sub-heading">' + data['headline'] + '</p>' + '<p class="sub-content">' + data['headline'] + '</p>' + '<p class="author-name">' + data['author-name'] + '</p>' + '<p class="author-name">' + 'Posted 15 mins ago' + '</p>' +  '<div class="col-md-4 travel-content"> </div><div class="col-md-3 publish"> </div> </div> </div>'
     
 }
 
